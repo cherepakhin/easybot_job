d156e407a3863dcb4499c1f69c7691654986177e
Черепахин Василий <vasi.che@gmail.com>
2023-06-04 21:56:12 +0500

    fix error in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-insert into group_product(id, name, parent_id) values (1, 'Desktop Computers', false);
+insert into group_product(id, name, parent_id) values (1, 'Desktop Computers', -1);
 insert into group_product(id, name, parent_id) values (2, 'Notebooks', -1);
 insert into group_product(id, name, parent_id) values (3, 'Monitors', -1);
 insert into group_product(id, name, parent_id) values (4, 'Hard drives', -1);
