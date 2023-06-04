936517ac6cb4a1b021248a2142c4e093ef41a5ee
Черепахин Василий <vasi.che@gmail.com>
2023-06-04 22:22:42 +0500

    add GroupProductRestContollerTest.getById()

--- src/test/java/ru/perm/v/easybot/rest/GroupProductRestContollerTest.java
+++ src/test/java/ru/perm/v/easybot/rest/GroupProductRestContollerTest.java
@@ -8,7 +8,7 @@ import ru.perm.v.easybot.service.GroupProductService;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -24,11 +24,30 @@ class GroupProductRestContollerTest {
     @Test
     void getAll() {
         GroupProductService groupProductService = mock(GroupProductService.class);
-        List<GroupProductEntity> groups= new ArrayList<>();
+        List<GroupProductEntity> groups = new ArrayList<>();
         groups.add(new GroupProductEntity(1L, "NAME_1", true, -1L));
         when(groupProductService.getAll()).thenReturn(groups);
         GroupProductRestContoller contoller = new GroupProductRestContoller(groupProductService);
         List<GroupProductDTO> dtos = contoller.getAll();
         assertEquals(1, dtos.size());
+    }
+
+    @Test
+    void getById() throws Exception {
+        Long ID = 1L;
+        String NAME = "NAME_1";
+        Long PARENT_ID = 1L;
+        GroupProductService groupProductService = mock(GroupProductService.class);
+        GroupProductEntity groupProduct = new GroupProductEntity();
+        groupProduct.setId(ID);
+        groupProduct.setName(NAME);
+        groupProduct.setParentId(PARENT_ID);
+
+        when(groupProductService.getById(ID)).thenReturn(groupProduct);
+        GroupProductRestContoller contoller = new GroupProductRestContoller(groupProductService);
+
+        GroupProductDTO dto = contoller.getById(ID);
+
+        assertEquals(new GroupProductDTO(ID,NAME, PARENT_ID), dto);
     }
 }
