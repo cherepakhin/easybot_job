e43a17e4d284ecc39ced05c0e5524f8cde8f3617
Черепахин Василий <vasi.che@gmail.com>
2023-06-04 11:21:08 +0500

    chande group_product "id" to "n

--- src/main/resources/db/migration/V1_001__create_group_product.sql
+++
@@ -1,9 +0,0 @@
-CREATE table if not exists group_product
-(
-    "id"        int      not null UNIQUE,
-    "name"      varchar(20) not null default '',
-    "is_last"   boolean      not null default false,
-    "parent_id" int      not null default -1
-);
-
-COMMIT;
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into group_product(n, name, is_last, parent_id) values (1, 'Desktop Computers', true, false);
+insert into group_product(n, name, is_last, parent_id) values (2, 'Notebooks', true, -1);
+insert into group_product(n, name, is_last, parent_id) values (3, 'Monitors', true, -1);
+insert into group_product(n, name, is_last, parent_id) values (4, 'Hard drives', true, -1);
+
+COMMIT;
