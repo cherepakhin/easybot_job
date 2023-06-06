4010e9eb43f9568fca00707b146e59ee54d48f60
Черепахин Василий <vasi.che@gmail.com>
2023-06-07 00:32:28 +0500

    clear code

--- src/main/java/ru/perm/v/easybot/repository/GroupProductRepository.java
+++ src/main/java/ru/perm/v/easybot/repository/GroupProductRepository.java
@@ -1,6 +1,7 @@
 package ru.perm.v.easybot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.perm.v.easybot.entity.GroupProductEntity;
@@ -11,14 +12,11 @@ import java.util.List;
 @Transactional
 public interface GroupProductRepository extends JpaRepository<GroupProductEntity, Long> {
     List<GroupProductEntity> findAllByOrderByIdAsc();
-//    @Query(value = "SELECT max(id) FROM group_product")
-//    Long getMaxId();
 
-//    List<GroupProductEntity> findByOrderByParentIdAsc(Long parentId);
+    @Query(value = "SELECT max(id) FROM group_product")
+    Long getMaxId();
 
     List<GroupProductEntity> findByParentIdOrderByParentIdAsc(Long id);
 
     List<GroupProductEntity> findByParentIdOrderByParentIdAscIdAsc(Long id);
-//    List<GroupProductEntity> findAllSubGroupsByParentIdAndSortedByParentId(Long parentId);
-//    List<GroupProductEntity> findByOrderByParentIdAsc(Long id);
 }
