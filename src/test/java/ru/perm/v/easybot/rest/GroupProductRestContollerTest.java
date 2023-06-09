82535394688da79ba1e0e8065271e4484061d165
Черепахин Василий <vasi.che@gmail.com>
2023-06-09 12:55:02 +0500

    add GroupProductRestContollerTest.update

--- src/test/java/ru/perm/v/easybot/rest/GroupProductRestContollerTest.java
+++ src/test/java/ru/perm/v/easybot/rest/GroupProductRestContollerTest.java
@@ -75,4 +75,34 @@ class GroupProductRestContollerTest {
 
         assertEquals(new GroupProductDTO(ID, NAME, PARENT_ID, IS_LAST), dto);
     }
+
+    @Test
+    void update() throws Exception {
+        Long ID = 1L;
+        String NAME_OLD = "NAME_OLD";
+        String NAME_NEW = "NAME_NEW";
+        Long PARENT_ID = 1L;
+        Boolean IS_LAST = true;
+
+        GroupProductService mockGroupProductService = mock(GroupProductService.class);
+
+        GroupProductEntity groupProductOld = new GroupProductEntity(ID, NAME_OLD, IS_LAST, PARENT_ID);
+        when(mockGroupProductService.getById(ID)).thenReturn(groupProductOld);
+
+        GroupProductEntity parentGroupProduct = new GroupProductEntity(PARENT_ID, "", false, -1L);
+        when(mockGroupProductService.getById(PARENT_ID)).thenReturn(parentGroupProduct);
+
+//        when(groupProductService.create(NAME_NEW, PARENT_ID, IS_LAST)).thenReturn(groupProductNew);
+
+        GroupProductEntity groupProductNew = new GroupProductEntity(ID, NAME_NEW, IS_LAST, PARENT_ID);
+        when(mockGroupProductService.save(groupProductNew)).thenReturn(groupProductNew);
+        when(mockGroupProductService.save(ID, NAME_NEW, PARENT_ID, IS_LAST)).thenReturn(groupProductNew);
+
+        GroupProductRestContoller contoller = new GroupProductRestContoller(mockGroupProductService);
+
+        GroupProductDTO dto = contoller.update(ID, NAME_NEW, PARENT_ID, IS_LAST);
+
+        assertEquals(new GroupProductDTO(ID, NAME_NEW, PARENT_ID, IS_LAST), dto);
+    }
+
 }
