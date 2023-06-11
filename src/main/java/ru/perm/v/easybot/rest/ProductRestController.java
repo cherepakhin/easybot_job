53279c76c671c3214fa7494e654433051b3b3b9c
Черепахин Василий <vasi.che@gmail.com>
2023-06-11 19:04:25 +0500

    add ProductRestController.delete

--- src/main/java/ru/perm/v/easybot/rest/ProductRestController.java
+++ src/main/java/ru/perm/v/easybot/rest/ProductRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.perm.v.easybot.dto.ProductDTO;
 import ru.perm.v.easybot.entity.ProductEntity;
 import ru.perm.v.easybot.rest.excpt.BadRequestException;
+import ru.perm.v.easybot.rest.excpt.Err500Exception;
 import ru.perm.v.easybot.rest.validators.ValidatorProductDto;
 import ru.perm.v.easybot.service.ProductService;
 
@@ -41,7 +42,6 @@ public class ProductRestController {
         return new ProductDTO(entity.getId(), entity.getName(), entity.getGroupProductId());
     }
 
-    //TODO: Add cache
     @GetMapping("/")
     @ApiOperation("Get all ProductDTO")
     public List<ProductDTO> getAll() throws Exception {
@@ -68,6 +68,7 @@ public class ProductRestController {
     @PutMapping("/")
     @ApiOperation("Create ProductDTO")
     public ProductDTO create(@RequestBody ProductDTO productDTO) throws Exception {
+        log.info("Create %s", productDTO.toString());
         List<String> errors = validator.validate(productDTO);
         if (errors.size() > 0) {
             String errorMes = String.format("Product not created %s, Errors: %s", productDTO, errors.stream().collect(Collectors.joining()));
@@ -85,8 +86,21 @@ public class ProductRestController {
     @GetMapping("/groupId/{groupId}")
     @ApiOperation("Get ProductDTO by GroupId")
     public List<ProductDTO> getByGroupId(@PathVariable Long groupId) {
+        log.info("Get all products by groupId=%s", groupId);
         return productService.getByIdGroupProduct(groupId).stream()
                 .map(p -> new ProductDTO(p.getId(), p.getName(), p.getGroupProductId()))
                 .collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation("Delete ProductDTO by id")
+    public void delete(@PathVariable("id") Long id) {
+        log.info("Delete product %s", id);
+        try {
+            productService.delete(id);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new Err500Exception(e.getMessage());
+        }
     }
 }
