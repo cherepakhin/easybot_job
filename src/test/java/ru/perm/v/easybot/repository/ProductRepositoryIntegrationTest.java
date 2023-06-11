6b29d46aca935d8aa9852e78f5c8d88ec59df55d
Черепахин Василий <vasi.che@gmail.com>
2023-06-11 23:57:18 +0500

    ProductRepositoryIntegrationTest.createProduct()

--- src/test/java/ru/perm/v/easybot/repository/ProductRepositoryIntegrationTest.java
+++ src/test/java/ru/perm/v/easybot/repository/ProductRepositoryIntegrationTest.java
@@ -3,6 +3,7 @@ package ru.perm.v.easybot.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import ru.perm.v.easybot.entity.ProductEntity;
 
 import java.util.List;
@@ -10,10 +11,22 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Transactional
 class ProductRepositoryIntegrationTest {
 
     @Autowired
     public ProductRepository repository;
+
+    @Test
+    void createProduct() {
+        Long ID = repository.getMaxId() + 1;
+        Long GROUP_ID = 3L;
+        String NAME = "NAME";
+
+        ProductEntity product = new ProductEntity(ID, NAME, GROUP_ID);
+        ProductEntity saved = repository.save(product);
+        assertEquals(new ProductEntity(ID, NAME, GROUP_ID), saved);
+    }
 
     @Test
     void findByGroupProductIdOrderByIdAsc() {
@@ -23,4 +36,6 @@ class ProductRepositoryIntegrationTest {
         assertEquals("Desktop2", desktops.get(1).getName());
         assertEquals("Desktop3", desktops.get(2).getName());
     }
+
+
 }
