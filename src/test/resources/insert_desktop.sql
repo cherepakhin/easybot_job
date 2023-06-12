5bf7477c847bb6d67f3a01ddce56013c9e41d092
Черепахин Василий <vasi.che@gmail.com>
2023-06-12 15:41:13 +0500

    change insert_desktop.sql for  ProductRepositoryIntegrationTest

--- src/test/resources/insert_desktop.sql
+++ src/test/resources/insert_desktop.sql
@@ -1,3 +1,5 @@
 delete from product;
 insert into product(id, name, group_product_id) values (32, 'Desktop32', 3);
 insert into product(id,name, group_product_id) values (31,'Desktop31',3);
+
+insert into product(id,name, group_product_id) values (41,'Notebook41',4);
